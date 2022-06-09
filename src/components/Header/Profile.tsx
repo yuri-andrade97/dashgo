4e71485adf4d37b5a979eb3ebc7c5bdf3a8ff825
yuri.andrade <yuri.andarde@dbserver.com.br>
2022-06-09 17:15:26 -0300

    adicionando responsividade

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Yuri Andrade</Text>
-                <Text
-                    color="gray.300"
-                    fontSize="small"
-                >
-                    dev.yuri97@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Yuri Andrade</Text>
+                    <Text
+                        color="gray.300"
+                        fontSize="small"
+                    >
+                        dev.yuri97@gmail.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Yuri Andrade" />
         </Flex>
